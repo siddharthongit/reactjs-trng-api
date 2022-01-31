ef5dc82f012a972ae929d1948e7387b6f3ea6f8a
Siddharth Sharma <siddharth54330@gmail.com>
2022-02-01 01:40:10 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+
+	
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+
+		fetch(
+			"https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((json) => {
+					this.setState({
+						items: json,
+						DataisLoaded: true
+					});
+				})
+		  console.log("first componenetdidmount");
+
+	}
+
+
+
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				items.map((item) => (
+				<ol key = { item.id } >
+					User_Name: { item.username },
+					Full_Name: { item.name },
+					User_Email: { item.email }
+					</ol>
+				))
+			}
+		</div>
+	);
+
 }
+
+
+
+
+
+
+
+
+}
+
+
+function call_get_api(){
+
+	// this is one of the api-get request // 
+	fetch(
+		"https://jsonplaceholder.typicode.com/users")
+		.then((res) => res.json())
+		.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	  console.log("first componenetdidmount");
+}
+
+
+function call_post_api(){
+		fetch('https://mywebsite.com/endpoint/', {
+	method: 'POST',
+	headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		firstParam: 'yourValue',
+		secondParam: 'yourOtherValue',
+	})
+	})
+}
+
+
 
 export default App;
